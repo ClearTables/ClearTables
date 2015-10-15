2e40aded050f8fa2d1a1e43cc8a0f03d72271298
Paul Norman <penorman@mac.com>
2015-10-14 23:38:43 -0700

    reorder water area tests

--- test-water.lua
+++ test-water.lua
@@ -1,5 +1,6 @@
 --[[
   This file is part of ClearTables
+
   @author Paul Norman <penorman@mac.com>
   @copyright 2015 Paul Norman, MIT license
 ]]--
@@ -7,11 +8,11 @@
 require "water"
 
 print("TESTING: accept_water_area")
+assert(not accept_water_area({}), "test failed: untagged")
+assert(not accept_water_area({foo="bar"}), "test failed: other tags")
 assert(accept_water_area({natural="water"}), "test failed: natural=water")
 assert(accept_water_area({waterway="riverbank"}), "test failed: waterway=riverbank")
 assert(accept_water_area({landuse="reservoir"}), "test failed: landuse=reservoir")
-assert(not accept_water_area({}), "test failed: untagged")
-assert(not accept_water_area({foo="bar"}), "test failed: other tags")
 
 print("TESTING: transform_water_area")
 assert(deepcompare(transform_water_area({}), {}), "test failed: no tags")
