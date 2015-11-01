2ed9fa133536a9942e7d8ebd7e127cc26b41355b
Paul Norman <penorman@mac.com>
2015-11-01 15:33:42 -0800

    Add a script for generating sql COMMENTs

---
+++ createcomments.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+
+import sys, yaml, re
+
+definitions = yaml.safe_load(sys.stdin)
+
+# Doesn't matter on newer versions, but prevents the use of \ in normal string literals
+print ("SET standard_conforming_strings = TRUE;");
+
+# Do everything in one transaction so we don't get some comments but not others
+print ("BEGIN;");
+
+for table in definitions:
+    # This requires all tables to be reasonably named
+    if not re.match('''^[a-zA-Z0-9_ ]+$''', table["name"]):
+        sys.exit("Unsafe table name: " + table["name"])
+
+    for column in table["tags"]:
+        # Only check columns that have a comment
+        if "comment" in column:
+            if not re.match('''^[a-zA-Z0-9_ ]+$''', column["name"]):
+                sys.exit('''Unsafe column name in table "''' + table["name"] + '''"."''' + column["name"] + '''"''')
+
+            # This is a slightly less restrictive character set, but the only one that needs escaping is '
+            if not re.match('''^[a-zA-Z0-9_ ()"!.']+$''', column["comment"]):
+                sys.exit('''Unsafe column comment for "''' + table["name"] +'''"."''' + column["name"] + '''"''')
+            print ('''COMMENT ON COLUMN "{}"."{}" IS '{}';'''.format(
+                table["name"], column["name"], column["comment"].replace("'", "''")))
+
+print ("COMMIT;");
