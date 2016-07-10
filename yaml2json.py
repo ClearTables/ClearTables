5987ed2ff06bba94c044167f30673c60b1009046
Paul Norman <penorman@mac.com>
2016-07-10 14:54:06 -0700

    Support python 3 exceptions

--- yaml2json.py
+++ yaml2json.py
@@ -2,6 +2,6 @@
 import sys, yaml, json
 try:
     json.dump(yaml.safe_load(sys.stdin), sys.stdout, indent=2, separators=(',', ': '))
-except yaml.YAMLError, e:
+except yaml.YAMLError as e:
     sys.stderr.write("YAML error:\n%s\n" % e)
     sys.exit(1)
