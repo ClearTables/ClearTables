57903d99b62f7c0be81ba5190c07cc246ea9f851
Paul Norman <penorman@mac.com>
2018-05-04 12:17:53 -0700

    Remove stray s in view names

--- sql/post/wikidata_shim.sql
+++ sql/post/wikidata_shim.sql
@@ -10,7 +10,7 @@ CREATE OR REPLACE VIEW planet_osm_line
 COMMENT ON VIEW planet_osm_point IS 'Wikidata ID of line features, in tags hstore column';
 CREATE INDEX planet_osm_line_wikidata ON wikidata_line (((hstore('wikidata', 'Q'||wikidata)) -> 'wikidata'));
 
-CREATE OR REPLACE VIEW planet_osm_polygons
+CREATE OR REPLACE VIEW planet_osm_polygon
   AS SELECT osm_id, hstore('wikidata', 'Q'||wikidata) AS tags, way
     FROM wikidata_polygon;
 COMMENT ON VIEW planet_osm_polygon IS 'Wikidata ID of polygon features, in tags hstore column';
